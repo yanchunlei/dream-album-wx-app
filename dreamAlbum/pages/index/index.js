5a963edc12bba70e815ba1e0d8ebfaae2dcfed05
ycl <y20080328@126.com>
2016-12-20 14:45:08 +0800

    极简版创建相册

--- dreamAlbum/pages/index/index.js
+++ dreamAlbum/pages/index/index.js
@@ -14,13 +14,62 @@ Page({
       url:'../viewswiper/viewswiper?albumId='+e.currentTarget.dataset.albumid
      })
   },
+  confirmUploadImage: function(e, tempFilePaths){
+    console.log(e,tempFilePaths)
+    let uploadfailed = false
+    let that = this
+    for(let i =0; i<tempFilePaths.length && !uploadfailed; i++){
+      wx.uploadFile({
+        url: app.globalData.serverHost + "/dream/album/common/uploaduserimg.json",
+        filePath: tempFilePaths[i],
+        name: 'image',
+        formData: {
+          'userId': this.userId,
+          'albumId': e.currentTarget.dataset.albumid,
+          'rank':i,
+          'complete': i===tempFilePaths.length-1?1:0
+        },
+        success: function (res) {
+          wx.hideToast()
+          if (i === tempFilePaths.length-1){
+            wx.redirectTo({
+              url: '../viewswiper/viewswiper?userAlbumId=' + res.userAlbumId
+            })
+          }
+        },
+        fail: function (res) {
+          that.requestFailed(res);
+          uploadfailed = true
+        }
+      })
+    }
+  },
   createImage:function(e){
-    //进入制作页面
-    wx.navigateTo({
-      url: '../create/create?albumId='+e.currentTarget.dataset.albumid
+    let that = this
+    wx.chooseImage({
+      count: e.currentTarget.dataset.totalitems,
+      success: function(res){
+        console.log(res)
+        if(res.tempFilePaths.length < e.currentTarget.dataset.totalitems){
+          wx.showModal({
+            title:"提示",
+            content: "该相册可以上传"+e.currentTarget.dataset.totalitems+"张照片，您选中"+res.tempFilePaths.length+"张 确认是否上传",
+            success: function(rescfm){
+              if(rescfm.confirm){
+                that.confirmUploadImage(e, res.tempFilePaths)
+              }else{
+                that.createImage(e)
+              }
+            }
+          })
+        }else{
+          that.confirmUploadImage(e, res.tempFilePaths)
+        }
+      }
     })
   },
   onLoad:function(options){
+    this.userId = wx.getStorageSync('userId');
     let that=this;
     that.setData({
       winWidth:app.globalData.windowWidth,
@@ -70,6 +119,7 @@ Page({
       },
       method: 'GET',
       success: function(res){
+        console.log(res)
         that.setData({
           items:that.data.items.concat(res.data.albumList),
           start:that.data.start+res.data.albumList.length,
